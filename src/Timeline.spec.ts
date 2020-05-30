70c0d921cab1d347f108d7708553959a7b755640
shortpoet <soriano.carlitos@gmail.com>
2020-05-30 11:41:53 -0500

    end video 5/6?

---
+++ src/Timeline.spec.ts
@@ -0,0 +1,46 @@
+import { mount } from "@vue/test-utils"
+import Timeline from './Timeline.vue'
+import { nextTick } from "vue"
+
+describe('Timeline.vue', () => {
+  it('renders 3 time periods', () => {
+    const wrapper = mount(Timeline)
+    expect(wrapper.findAll('[data-test="period"]')).toHaveLength(3)
+  })
+
+  it('updated the period when clicked', async () => {
+    const wrapper = mount(Timeline)
+    // using $ to represent DOM elements, going back to jQuery days
+    const $today = wrapper.findAll('[data-test="period"]')[0]
+    expect($today.classes()).toContain('is-active')
+    
+    const $thisWeek = wrapper.findAll('[data-test="period"]')[1]
+    await $thisWeek.trigger('click')
+
+    expect($today.classes()).not.toContain('is-active')
+    expect($thisWeek.classes()).toContain('is-active')
+
+    const $thisMonth = wrapper.findAll('[data-test="period"]')[2]
+    await $thisMonth.trigger('click')
+
+    expect($thisWeek.classes()).not.toContain('is-active')
+    expect($thisMonth.classes()).toContain('is-active')
+
+  })
+
+  it('renders todays post by default', async () => {
+    const wrapper = mount(Timeline)
+    expect(wrapper.findAll('[data-test="post"]')).toHaveLength(1)
+
+    const $thisWeek = wrapper.findAll('[data-test="period"]')[1]
+    await $thisWeek.trigger('click')
+
+    expect(wrapper.findAll('[data="post"]')).toHaveLength(2)
+
+    const $thisMonth = wrapper.findAll('[data-test="period"]')[2]
+    await $thisMonth.trigger('click')
+
+    expect(wrapper.findAll('[data="post"]')).toHaveLength(3)
+  })
+
+})
--- src/Timeline.vue
+++ src/Timeline.vue
@@ -1,20 +1,68 @@
 <template>
   <nav class="is-primary panel">
     <p class="panel-tabs">
-      <a href="" v-for="period in periods" :key="period">{{ period }}</a>
+      <!-- define a test specific selector so that future code changes to tag, class, or id, which don't nec change functionality, don't break test eg a => div -->
+      <a v-for="period in periods" :key="period" data-test="period"
+        :class="[ period === selectedPeriod ? 'is-active' : '']"
+        @click="setPeriod(period)"
+      >
+        {{ period }}
+      </a>
     </p>
+      <a data-test="post" href="" class="panel-block" v-for="post in posts" :key="post.id">
+        <div>
+          <a>{{ post.title }}</a>
+          <div>{{ post.created.format('Do MMM') }}</div>
+        </div>
+      </a>
   </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { Period } from './types'
+import { defineComponent, ref, computed } from 'vue'
+import { Period, Post } from './types'
+import {today, thisWeek, thisMonth} from './mocks'
+import moment=require('moment')
 
 export default defineComponent({
   setup() {
     const periods : Period[] = ['today', 'this week', 'this month']
+
+    // ref is generic type
+    const selectedPeriod = ref<Period>('today')
+
+    // computed automatically recalculates and updates the DOM anytime a reactive reference changes 
+    const posts = computed(() => [today, thisWeek, thisMonth].filter(post => {
+      if (
+        selectedPeriod.value === 'today' &&
+        post.created.isAfter(moment().subtract(1, 'day'))
+      ) {
+        return true
+      }
+      if (
+        selectedPeriod.value === 'this week' &&
+        post.created.isAfter(moment().subtract(1, 'week'))
+      ) {
+        return true
+      }
+      if (
+        selectedPeriod.value === 'this month' &&
+        post.created.isAfter(moment().subtract(1, 'month'))
+      ) {
+        return true
+      }
+      return false
+    })
+    )
+    const setPeriod = (period: Period) => {
+      selectedPeriod.value = period
+    }
+
     return {
-      periods
+      periods,
+      selectedPeriod,
+      setPeriod,
+      posts
     }
   }
 })
