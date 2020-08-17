26188fde60f67825750ffac11e9270d1ade323e5
shortpoet <soriano.carlitos@gmail.com>
2020-08-17 16:58:43 -0500

    add static folder

--- client/utils/colorLog.ts
+++ client/utils/colorLog.ts
@@ -9,52 +9,58 @@ type Options = {
 
 export function colorLog(message: string, options?: number): void;
 export function colorLog(message: string, options?: (number | Options)): void {
+  console.log('color log 1');
+  
   let color;
   let background;
   let _options: Options = {} as Options
   // use strict null check double bang to account for !0 being true
   // but then !!1 is true
   if (options == null) {
+    console.log('color log 2');
     _options.color = "magenta";
     _options.background = "yellow";
     options = _options;
   }
   if (typeof options == "object") {
+    console.log('color log 3');
     color = options.color;
     background = options.background;
   } else if (typeof options == "number") {
+    console.log('color log 4');
     switch (options) {
       case 1:
         color = "green";
         background = "yellow";
         break;
         
-      default:
-        color = "magenta";
-        background = "yellow";
-      break;
-    }
-  }
-
+        default:
+          color = "magenta";
+          background = "yellow";
+          break;
+        }
+      }
+      
+  console.log('color log 5');
   // https://stackoverflow.com/a/37081135/12658653
   if(process.env.NODE_ENV != 'production') {
     const e = new Error();
     if (!e.stack)
-        try {
-            // IE requires the Error to actually be thrown or else the 
-            // Error's 'stack' property is undefined.
-            throw e;
+    try {
+      // IE requires the Error to actually be thrown or else the 
+      // Error's 'stack' property is undefined.
+      throw e;
         } catch (e) {
             if (!e.stack) {
-                //return 0; // IE < 10, likely
+              //return 0; // IE < 10, likely
             }
-        }
-    const stack = e.stack.toString().split(/\r\n|\n/);
-    if (message === '') {
-        message = '""';
+          }
+          const stack = e.stack.toString().split(/\r\n|\n/);
+          if (message === '') {
+            message = '""';
     }
     // message = `${message}\n${stack[1]}`
-  
+    
     // console.log(`%c` + `${message}`, `color:` + `${color};background:${background}`)
     const trace = stack[1].split('src/')
     // const file = trace[1].match(/(.+?):/)[1]
@@ -64,12 +70,13 @@ export function colorLog(message: string, options?: (number | Options)): void {
     const line = trace[1].match(/(:.*)/)[0]
     console.log(`%c${message}%c\t\t\t\t\t${file}%c${line}`, `color:${color};background:${background}`, `color:#003EC5;background:white;text-align:right;` ,`color:#0086E1;background:white;text-align:right;`)
   } else {
+    console.log('color log 6');
     console.log(`%c` + `${message}`, `color:` + `${color};background:${background}`)
   }
   
 }
 
 // module.exports =  {
-//   log,
-//   colorLog
+  //   log,
+  //   colorLog
 // }
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,16 +2,26 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
 const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 // from console warning
 // https://github.com/vuejs/vue-next/tree/master/packages/vue#bundler-build-feature-flags
 // https://webpack.js.org/plugins/define-plugin/
-// const vueBundlerBuildFeatureFlagsPlugin = new webpack.DefinePlugin({
-//   '__VUE_OPTIONS_API__': JSON.stringify(true),
-//   '__VUE_PROD_DEVTOOLS_': JSON.stringify(false)
-// });
+const vueBundlerBuildFeatureFlagsPlugin = new webpack.DefinePlugin({
+  '__VUE_OPTIONS_API__': JSON.stringify(true),
+  '__VUE_PROD_DEVTOOLS_': JSON.stringify(false)
+});
+
+// https://stackoverflow.com/a/33374807/12658653
+const copyWebpackPlugin = new CopyWebpackPlugin({
+  patterns: [
+    { from: 'static' }
+  ]
+})
 
 module.exports = {
+  // https://webpack.js.org/configuration/entry-context/#root
+  context: path.resolve(__dirname, './'),
   entry: './src/main.ts',
   output: {
     path: path.resolve(__dirname, '../docs/'),
@@ -42,12 +52,13 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new VueLoaderPlugin(),
-    // vueBundlerBuildFeatureFlagsPlugin
+    vueBundlerBuildFeatureFlagsPlugin,
+    copyWebpackPlugin
   ],
   devServer: {
     overlay: true,
